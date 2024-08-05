655846d6a1644abbd3023422727a0635f6f25fbf
kretoff <20kretovartem000@gmail.com>
2024-08-05 17:53:42 +0300

    add send, edit, delete message. Add block/unblock user (unworking)

--- core.py
+++ core.py
@@ -16,7 +16,7 @@ with open('core_config.json', 'r') as f:
     config = json.load(f)
 print('server started')
 def check_user(userId = "kretoffi", deviceID = "1", message = None):
-    return 0
+    return message
 def sql_conn():
     try:
         conn = sql.connect(
@@ -31,15 +31,29 @@ def sql_conn():
         print("No conaction SQL...")
         print(ex)
         return -1
+def chat_sql_conn(chatID = None):
+    try:
+        conn = sql.connect(
+            host=config['SQL_IP'],
+            port=config['SQL_PORT'],
+            user=config['SQL_USER'],
+            password=config['SQL_PASSWORD'],
+            database=chatID
+        )
+        return conn
+    except Exception as ex:
+        print("No conaction SQL...")
+        print(ex)
+        return -1
 
 app = FastAPI()
 
 conn = sql_conn()
 cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, userID varchar(8), login varchar(50), tag varchar(50), password varchar(50), email varchar(50), subtitle varchar(50), num varchar(15), dayOfBirthdae varchar(50))')
-cur.execute('CREATE TABLE IF NOT EXISTS chatsList (id int auto_increment primary key, chatID varchar(64), user1ID varchar(8), user2ID varchar(8))')
-cur.execute('CREATE TABLE IF NOT EXISTS staticKeys (id int auto_increment primary key, userID varchar(8), deviceID int, staticKey varchar(128), timeReg int)')
-cur.execute('CREATE TABLE IF NOT EXISTS conf (id int auto_increment primary key,userID varchar(8), viewNumber bit, viewTime bit, viewEmail bit, viewIcon bit, resandMessage bit, collin bit, gs bit, sendMessage bit, viewDateOfBirthday bit, viewSubtitle bit)')
+cur.execute('CREATE TABLE IF NOT EXISTS users (userID varchar(8) primary key, login varchar(50), tag varchar(50), password varchar(50), email varchar(50), subtitle varchar(50), num varchar(15), dayOfBirthdae varchar(50))')
+cur.execute('CREATE TABLE IF NOT EXISTS chatsList (chatID varchar(64) primary key, user1ID varchar(8), user2ID varchar(8))')
+cur.execute('CREATE TABLE IF NOT EXISTS staticKeys (userID varchar(8), deviceID int, staticKey varchar(128), timeReg int)')
+cur.execute('CREATE TABLE IF NOT EXISTS conf (userID varchar(8) primary key, viewNumber bit, viewTime bit, viewEmail bit, viewIcon bit, resandMessage bit, collin bit, gs bit, sendMessage bit, viewDateOfBirthday bit, viewSubtitle bit)')
 conn.commit()
 cur.close()
 
@@ -78,7 +92,7 @@ def reg(cod, tag, password, email, id):
         conn.commit()
         cur.close()
         return {"data": "invalidEmail"}
-    if cod == temp["users_reg_codes"][id]:
+    if cod == temp["users_reg_codes"][str(id)]:
         staticKey = generate_random_string(128)
         id = generateUserID()
         cur.execute('INSERT INTO users (tag, password, email, userID) VALUES ("%s", "%s", "%s", "%s")' % (tag, password, email, id))
@@ -86,9 +100,8 @@ def reg(cod, tag, password, email, id):
         conn.commit()
         cur.close()
         conn.close()
-        return {"data": "goodRegistration", "tag":tag, "email":email, "id":id, "key":staticKey}
+        return {"data": "goodRegistration", "tag":tag, "email":email, "id":id, "key":staticKey, "deviceID":0}
     else:
-        user.send('wrongCode'.encode('utf-8'))
         return {"data": "wrongCode"}
 def generateUserID():
     id = generate_random_string(8)
@@ -99,6 +112,15 @@ def generateUserID():
     if r == None or len(r) <= 0:
         return id
     else: return generateUserID()
+def generateChatID():
+    id = generate_random_string(64)
+    conn = sql_conn()
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM chatsList WHERE chatID = %s', (id,))
+    r = cur.fetchall()
+    if r == None or len(r) <= 0:
+        return id
+    else: return generateChatID()
 def log(tag, password):
     conn = sql_conn()
     cur = conn.cursor()
@@ -127,7 +149,18 @@ def see_chat_list(userID):
         data["chatList"][el[1]] = {}
         data["chatList"][el[1]]["user1ID"] = el[2]
         data["chatList"][el[1]]["user2ID"] = el[3]
+        data["chatList"][el[1]]["lastMessage"] = get_messges(1,1)
+    cur.close()
+    conn.close()
+    return {"chatList":{"0000000000000001":{"user1ID":"kretoffi", "user2ID":"llllllll", "lastMessage":"kretoff go to me"},"1111111111111211":{"user1ID":"kretoffi", "user2ID":"llllllld", "lastMessage":"kretoff go to me lol"}}}
     return data
+
+def get_messges(fromI, toI):
+    conn = sql_conn()
+    cur = conn.cursor()
+    cur.close()
+    conn.close()
+    return ["no_messages"]
 @app.get("/test")
 def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
     message = check_user(my_id, deviceID)
@@ -138,16 +171,26 @@ def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[
 def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
     message = check_user(my_id, deviceID)
     if message == -1: return {"data": "verifityError"}
-    return {"chats": see_chat_list(data["my_id"])}
+    return see_chat_list(my_id)
 @app.get("/")
 def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
     message = check_user(my_id, deviceID)
     if message == -1: return {"data": "verifityError"}
     return {"your_id":my_id, "deviceID":deviceID}
 
+@app.get('/allUsersCol')
+def main():
+    conn = sql_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM users')
+    count = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return {"count": count}
+
 @app.post("/sandReg")
 def main(data = Body()):
-    #try:
+    try:
         authCode = str(random.randint(100000, 999999))
         user_id = len(temp["users_reg_codes"])
         temp["users_reg_codes"][str(user_id)] = authCode
@@ -155,8 +198,8 @@ def main(data = Body()):
         _email = data["email"]
         send_email(f"Ваш код авторизации: {authCode}. Здравствуйте, на этот адрес электронной почты пытаются зарегестрировать аккаунт в krager. Если это не вы, просто проигнорируйте это сообщение",_email)
         return {"data": "message_sanded", "your_id": user_id}
-    #except Exception as _ex:
-        #return {"data": f"error {_ex}"}
+    except Exception as _ex:
+        return {"data": f"error {_ex}"}
 @app.post("/reg")
 def main(data = Body()):
     return reg(data["cod"], data["tag"], data["password"], data["email"], data["id"])
@@ -165,7 +208,7 @@ def main(data = Body()):
     return log(data["tag"], data["password"])
 @app.post("/sandMessage")
 def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
-    message = check_user(my_id, deviceID)
+    message = check_user(my_id, deviceID, data)
     if message == -1: return {"data": "verifityError"}
     return
 
@@ -174,3 +217,77 @@ def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[
     message = check_user(my_id, deviceID)
     if message == -1: return {"data": "verifityError"}
     return
+
+@app.post("/chat/sandMessage")
+def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
+    message = check_user(my_id, deviceID, data)
+    if message == -1: return {"data": "verifityError"}
+    conn = chat_sql_conn("chats")
+    cur = conn.cursor()
+    chatID = None
+    print(message)
+    print(message["chatID"])
+    if message["chatID"] == None:
+        chatID = generateChatID()
+        cur.execute("CREATE TABLE IF NOT EXISTS %s (id int auto_increment primary key, senderID varchar(8), timeOfSend int, edit bit, see bit, message varchar(4096), mediaInfo varchar(1024))" % (chatID))
+    else:chatID = message["chatID"]
+    mediaInfo = None
+    cur.execute("INSERT INTO %s (senderId, timeOfSend, edit, see, message, mediaInfo) VALUES ('%s',%s,%s,%s,'%s','%s')" % (chatID,my_id, int(time.time()), 0, 0, message['message'], mediaInfo))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"data":"messageSanded"}
+@app.post("/chat/editMessage")
+def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
+    message = check_user(my_id, deviceID, data)
+    if message == -1: return {"data": "verifityError"}
+    conn = chat_sql_conn("chats")
+    cur = conn.cursor()
+    cur.execute("UPDATE %s SET message = '%s', edit = %s WHERE id = '%s'" % (data["chatID"], data["message"], 1, data["message_id"]))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"data":"messageEdited"}
+
+@app.post("/chat/delMessage")
+def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
+    message = check_user(my_id, deviceID, data)
+    if message == -1: return {"data": "verifityError"}
+    conn = chat_sql_conn("chats")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM %s WHERE id = %s" % (message["chatID"],message["message_id"]))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"data":"messageDeleted"}
+@app.post("/blockUser")
+def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
+    message = check_user(my_id, deviceID, data)
+    if message == -1: return {"data": "verifityError"}
+    if message["userID"] == "kretoffi": return {"data":"userDon'tBlocked"}
+    conn = chat_sql_conn(my_id)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM blockUsers WHERE userID = ?", (data['userID']))
+    t = cur.fetchone()
+    if t != None or len(t) >= 0: return {"data": "userBeBlocked"}
+    cur.execute("CREATE TABLE IF NOT EXISTS blockUsers(id int auto_increment primary key, userID varchar(8), timeTo int)")
+    cur.execute("INSERT INTO blockUsers (userID, timeTo) VALUES ('%s', '%s')" % (message["userID"], message["timeTo"]))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"data":"userBlocked"}
+@app.post("/unblockUser")
+def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
+    message = check_user(my_id, deviceID, data)
+    if message == -1: return {"data": "verifityError"}
+    conn = chat_sql_conn(my_id)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM blockUsers WHERE userID = ?", (data['userID']))
+    t = cur.fetchone()
+    if t == None or len(t) == 0: return {"data":"userDon'tBeBlocked"}
+    cur.execute("DELETE FROM blockUsers WHERE id = ?", (message["userID"]))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"data":"userUnblocked"}
+
