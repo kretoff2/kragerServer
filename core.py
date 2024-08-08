4f742b7b18a9c1b0c863b467ae5ca7947880fd8d
kretoff <20kretovartem000@gmail.com>
2024-08-08 11:00:04 +0300

    Add /chat/info. Debuging. Test send message, all good

--- core.py
+++ core.py
@@ -50,7 +50,7 @@ app = FastAPI()
 
 conn = sql_conn()
 cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS users (userID varchar(8) primary key, login varchar(50), tag varchar(50), password varchar(50), email varchar(50), subtitle varchar(50), num varchar(15), dayOfBirthdae varchar(50))')
+cur.execute('CREATE TABLE IF NOT EXISTS users (userID varchar(8) primary key, login varchar(50), tag varchar(50), password varchar(50), email varchar(50), subtitle varchar(50), num varchar(15), dayOfBirthday varchar(50))')
 cur.execute('CREATE TABLE IF NOT EXISTS chatsList (chatID varchar(64) primary key, user1ID varchar(8), user2ID varchar(8))')
 cur.execute('CREATE TABLE IF NOT EXISTS staticKeys (userID varchar(8), deviceID int, staticKey varchar(128), timeReg int)')
 cur.execute('CREATE TABLE IF NOT EXISTS conf (userID varchar(8) primary key, viewNumber bit, viewTime bit, viewEmail bit, viewIcon bit, resandMessage bit, collin bit, gs bit, sendMessage bit, viewDateOfBirthday bit, viewSubtitle bit)')
@@ -132,12 +132,12 @@ def log(tag, password):
         return {"data": "invalidLoginOrPassword"}
     else:
         staticKey = generate_random_string(128)
-        cur.execute('SELECT * FROM statickeys WHERE userID = %s', (thisUser[1],))
+        cur.execute('SELECT * FROM statickeys WHERE userID = %s', (thisUser[0],))
         d = cur.fetchall()
-        cur.execute('INSERT INTO statickeys (userID, deviceID, staticKey, timeReg) VALUES ("%s","%s","%s", "%s")' % (thisUser[1], len(d), staticKey, int(time.time())))
+        cur.execute('INSERT INTO statickeys (userID, deviceID, staticKey, timeReg) VALUES ("%s","%s","%s", "%s")' % (thisUser[0], len(d), staticKey, int(time.time())))
         conn.commit()
         cur.close()
-        return {"data": "goodLogin", "userInfo":{"userID":thisUser[1], "login":thisUser[2], "tag":thisUser[3], "email":thisUser[5],"subTitle":thisUser[6], "num":thisUser[7]}, "key":staticKey, "deviceID":len(d)}
+        return {"data": "goodLogin", "userInfo":{"userID":thisUser[0], "login":thisUser[1], "tag":thisUser[2], "email":thisUser[4],"subTitle":thisUser[5], "num":thisUser[6]}, "key":staticKey, "deviceID":len(d)}
 
 def see_chat_list(userID):
     conn = sql_conn()
@@ -146,13 +146,12 @@ def see_chat_list(userID):
     chats = cur.fetchall()
     data = {"chatsCol":f"{len(chats)}","chatList":{}}
     for el in chats:
-        data["chatList"][el[1]] = {}
-        data["chatList"][el[1]]["user1ID"] = el[2]
-        data["chatList"][el[1]]["user2ID"] = el[3]
-        data["chatList"][el[1]]["lastMessage"] = get_messges(1,1)
+        data["chatList"][el[0]] = {}
+        data["chatList"][el[0]]["user1ID"] = el[1]
+        data["chatList"][el[0]]["user2ID"] = el[2]
+        data["chatList"][el[0]]["lastMessage"] = get_messges(1,1)
     cur.close()
     conn.close()
-    return {"chatList":{"0000000000000001":{"user1ID":"kretoffi", "user2ID":"llllllll", "lastMessage":"kretoff go to me"},"1111111111111211":{"user1ID":"kretoffi", "user2ID":"llllllld", "lastMessage":"kretoff go to me lol"}}}
     return data
 
 def get_messges(fromI, toI):
@@ -213,7 +212,7 @@ def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), devic
     return
 
 @app.get("/getMessages")
-def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID"), fromIndex: Optional[str] = Header(None, alias="fromIndex")):
+def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID"),chatID: Optional[str] = Header(None, alias="chatID"), fromIndex: Optional[str] = Header(None, alias="fromIndex")):
     message = check_user(my_id, deviceID)
     if message == -1: return {"data": "verifityError"}
     return
@@ -225,18 +224,27 @@ def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), devic
     conn = chat_sql_conn("chats")
     cur = conn.cursor()
     chatID = None
-    print(message)
-    print(message["chatID"])
     if message["chatID"] == None:
         chatID = generateChatID()
         cur.execute("CREATE TABLE IF NOT EXISTS %s (id int auto_increment primary key, senderID varchar(8), timeOfSend int, edit bit, see bit, message varchar(4096), mediaInfo varchar(1024))" % (chatID))
+        conn.commit()
+        cur.close()
+        conn.close()
+        conn = sql_conn()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO chatsList (chatID, user1ID, user2ID) VALUES ('%s', '%s', '%s')" % (chatID, my_id, message["user2ID"]))
+        conn.commit()
+        cur.close()
+        conn.close()
+        conn = chat_sql_conn("chats")
+        cur = conn.cursor()
     else:chatID = message["chatID"]
     mediaInfo = None
     cur.execute("INSERT INTO %s (senderId, timeOfSend, edit, see, message, mediaInfo) VALUES ('%s',%s,%s,%s,'%s','%s')" % (chatID,my_id, int(time.time()), 0, 0, message['message'], mediaInfo))
     conn.commit()
     cur.close()
     conn.close()
-    return {"data":"messageSanded"}
+    return {"data":"messageSanded", "chatID":chatID}
 @app.post("/chat/editMessage")
 def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID")):
     message = check_user(my_id, deviceID, data)
@@ -294,6 +302,7 @@ def main(data = Body(),my_id: Optional[str] = Header(None, alias="my_id"), devic
 @app.get("/search/user")
 def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID"), search: Optional[str] = Header(None, alias="search")):
     message = check_user(my_id, deviceID)
+    if message == -1: return {"data": "verifityError"}
     conn = sql_conn()
     cur = conn.cursor()
     cur.execute("SELECT * FROM users WHERE login = '%s' OR tag = '%s' OR userID = '%s' OR email = '%s' OR num = '%s'" % (search, search, search, search, search))
@@ -303,11 +312,33 @@ def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[
         data["results"][el[0]] = {}
         data["results"][el[0]]["name"] = el[1]
         data["results"][el[0]]["tag"] = el[2]
+        data["results"][el[0]]["chat"] = None
         cur.execute('SELECT * FROM chatsList WHERE user1ID = "%s" AND user2ID = "%s" OR user1ID = "%s" AND user2ID = "%s"' % (my_id, el[0], el[0], my_id))
         chat = cur.fetchone()
         if chat != None and len(chat)>=1:
-            data["results"][el[0]]["chat"] = chat
+            data["results"][el[0]]["chat"] = chat[0]
     cur.close()
     conn.close()
     return data
-
+@app.get("/chat/info")
+def main(my_id: Optional[str] = Header(None, alias="my_id"), deviceID: Optional[str] = Header(None, alias="deviceID"), chatID: Optional[str] = Header(None, alias="chatID")):
+    message = check_user(my_id, deviceID, chatID)
+    if message == -1: return {"data": "verifityError"}
+    conn = sql_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM chatsList WHERE chatID = "%s"' % (message))
+    r = cur.fetchone()
+    u = None
+    if r[1]==my_id:u=r[2]
+    else: u=r[1]
+    cur.execute('SELECT * FROM users WHERE userID = "%s"' % (message))
+    r = cur.fetchone()
+    cur.close()
+    conn.close()
+    conn = chat_sql_conn("chats")
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM %s" % ())
+    col = cur.fetchone()
+    cur.close()
+    conn.close()
+    return {"data":"good", "tag":r[2], "subtitle":r[5], "dayOfBirthday":r[7], "messageCol":col}
